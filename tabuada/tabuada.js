af2d2700a1f535491ea87b9b5c0b60ecfa2181aa
Mario Azevedo <mariobct@msn.com>
2025-02-19 18:40:58 -0300

    Adicionando tabuada

---
+++ tabuada/tabuada.js
@@ -0,0 +1,18 @@
+function clicar() {
+
+    const numero = document.getElementById('numero').value
+    const resultado = document.getElementById('resultado')
+    
+    
+    resultado.innerHTML = ''
+    
+    if (numero === '') {
+        resultado.innerHTML += '<p>Por favor, digite um número!</p>'
+        resultado.style.color = 'red'
+        return
+    }
+    
+    for (let i = 1; i <= 10; i++) {
+        resultado.innerHTML += `<p>${numero} x ${i} = ${numero * i}</p>`
+    }
+}
